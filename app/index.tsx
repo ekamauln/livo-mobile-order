89168a9ef71da30d29e89cab36c1322f0a1348e6
rifqyandika <andikakiky87@gmail.com>
2025-12-18 12:25:34 +0700

    add dashbord picker new style

--- app/index.tsx
+++ app/index.tsx
@@ -1,101 +1,133 @@
 import { useAuth } from "@/contexts/AuthContext";
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import api from "@/services/api";
+import { Order } from "@/types/order";
+import { useFocusEffect } from "@react-navigation/native";
+import { router } from "expo-router";
+import React, { useCallback, useState } from "react";
 import {
   ActivityIndicator,
-  Alert,
+  FlatList,
+  GestureResponderEvent,
   Image,
-  Keyboard,
-  KeyboardAvoidingView,
-  Platform,
+  RefreshControl,
   Text,
-  TextInput,
   TouchableOpacity,
-  TouchableWithoutFeedback,
-  View,
+  View
 } from "react-native";
 
-export default function LoginScreen() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const { login } = useAuth();
-  const handleLogin = async () => {
-    if (!username.trim() || !password.trim()) {
-      Alert.alert("Missing Info", "Please enter username and password");
-      return;
-    }
-    setIsSubmitting(true);
+const LogoutIcon = require('@/assets/v1/logout.png');
+const PackIcon = require('@/assets/v1/parcel.png');
+
+
+
+export default function PickerDashboard() {
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const { logout } = useAuth();
+
+  const fetchOrders = useCallback(async () => {
     try {
-      await login(username, password);
-    } catch (err) {
-      console.error(err);
+      const res = await api.get("/mobile/orders");
+      setOrders(res.data.data);
+    } catch (error) {
+      console.error("Failed to fetch orders:", error);
     } finally {
-      setIsSubmitting(false);
+      setLoading(false);
     }
-  };
-  return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        className="flex-1 bg-[#FF544B] justify-center"
-      >
-        <StatusBar style="light" />
-        <View className="px-5 w-full">
-          <View className="items-center mb-8">
-            <Image
-              source={require("@/assets/v1/logo.png")}
-              resizeMode="contain"
-              className="w-32 h-28 mb-1"
-            />
-            <Text className="text-white text-[28px] font-bold">
-              WMSRibbon
-            </Text>
-            <Text className="text-white text-xs opacity-90">
-              Warehouse Management System
-            </Text>
-          </View>
-          <View className="mb-2">
-            <TextInput
-              className="bg-white rounded-lg px-4 py-5 text-base text-gray-800"
-              placeholder="Email or Username"
-              placeholderTextColor="#B0B0B0"
-              value={username}
-              onChangeText={setUsername}
-              autoCapitalize="none"
-            />
-          </View>
-          <View className="mb-2">
-            <TextInput
-              className="bg-white rounded-lg px-4 py-5 text-base text-gray-800"
-              placeholder="Password"
-              placeholderTextColor="#B0B0B0"
-              value={password}
-              onChangeText={setPassword}
-              secureTextEntry
-            />
-          </View>
-          <TouchableOpacity
-            onPress={handleLogin}
-            disabled={isSubmitting}
-            className={`rounded-lg py-5 items-center mt-2 ${isSubmitting ? "bg-black/70" : "bg-black"
-              }`}
-          >
-            {isSubmitting ? (
-              <ActivityIndicator color="#fff" />
-            ) : (
-              <Text className="text-white text-lg font-bold">
-                Login
-              </Text>
-            )}
-          </TouchableOpacity>
-          <View className="items-center mt-2">
-            <Text className="text-white text-xs opacity-80">
-              Version 1.0.2
-            </Text>
-          </View>
+  }, []);
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    try {
+      const res = await api.get("/mobile/orders");
+      setOrders(res.data.data);
+    } catch (error) {
+      console.error("Failed to refresh orders:", error);
+    } finally {
+      setRefreshing(false);
+    }
+  }, []);
+
+  // Refetch when page is focused
+  useFocusEffect(
+    useCallback(() => {
+      fetchOrders();
+    }, [fetchOrders])
+  );
+
+  if (loading) {
+    return (
+      <View className="flex-1 p-4 ">
+        <Text className="text-2xl font-bold mb-4 ">My Assignments</Text>
+        <View className="flex-1 justify-center items-center">
+          <ActivityIndicator size="large" color="#0000ff" />
+          <Text className="mt-2">Loading...</Text>
         </View>
-      </KeyboardAvoidingView>
-    </TouchableWithoutFeedback>
+      </View>
+    );
+  }
+
+  const handleLogoutpress = (e?: GestureResponderEvent) => {
+    logout();
+  }
+
+  return (
+    <View className="flex-1 p-5 bg-[#F6F6F6]">
+
+      <View className="flex-row justify-between items-center mb-4">
+        <Text className="text-xl font-semibold text-gray-900">My Assignments</Text>
+
+        <TouchableOpacity 
+          onPress={logout}
+          className="w-10 h-10 rounded-full justify-center items-center">
+          <Image 
+            source={LogoutIcon}
+            className="w-6 h-6"
+            resizeMode="contain"
+            />
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={orders}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            className=" bg-white mb-3 p-4 rounded-lg shadow flex-row items-center"
+            onPress={() => router.push(`/picker/order/${item.id}`)}
+          >
+            <View className="w-10 h-10 rounded-lg justify-center items-center">
+              <Image
+              source={PackIcon}
+              className="w-10 h-10"
+              resizeMode="contain"
+              />
+            </View>
+            <View className="flex-1 ml-3">
+            <Text className="font-bold text-lg" >Order #{item.id}</Text>
+            <Text className="text-sm text-[#F2695D]">{item.tracking}</Text>
+            </View>
+            <TouchableOpacity className="p-2">
+              <Image
+              source={require('@/assets/v1/more.png')}
+              className="w-5 h-5"
+              />
+            </TouchableOpacity>
+          </TouchableOpacity>
+        )}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            colors={["#2196F3"]}
+          />
+        }
+        ListEmptyComponent={
+          <View className="flex justify-center align-middle py-50">
+            <Text className="text-gray-500 text-base">No orders assigned</Text>
+          </View>
+        }
+      />
+    </View>
   );
 }
