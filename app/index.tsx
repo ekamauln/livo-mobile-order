beba5f6dbef05f69c32dc63164d7e6aa2bf0034c
rifqyandika <andikakiky87@gmail.com>
2025-12-18 11:20:24 +0700

    back-setup

--- app/index.tsx
+++ app/index.tsx
@@ -38,7 +38,7 @@ export default function LoginScreen() {
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
-        className="flex-1 bg-[#09085a] justify-center"
+        className="flex-1 bg-[#FF544B] justify-center"
       >
         <StatusBar style="light" />
         <View className="px-5 w-full">
